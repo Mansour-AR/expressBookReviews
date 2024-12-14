b7d27202572974416800adf68471f7b2619ba2f7
Mansour-AR <mansour.aria96@hotmail.com>
2024-12-14 15:50:25 +0100

    Update booksdb.js

--- final_project/router/booksdb.js
+++ final_project/router/booksdb.js
@@ -1,14 +1,15 @@
 let books = {
-      1: {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": {} },
-      2: {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": {} },
-      3: {"author": "Dante Alighieri","title": "The Divine Comedy", "reviews": {} },
-      4: {"author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": {} },
-      5: {"author": "Unknown","title": "The Book Of Job", "reviews": {} },
-      6: {"author": "Unknown","title": "One Thousand and One Nights", "reviews": {} },
-      7: {"author": "Unknown","title": "Nj\u00e1l's Saga", "reviews": {} },
-      8: {"author": "Jane Austen","title": "Pride and Prejudice", "reviews": {} },
-      9: {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": {} },
-      10: {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": {} }
+      1: {"author": "Chinua Achebe","title": "Things Fall Apart", "isbn": "978-3-16-148410-0", "reviews": {} },
+      2: {"author": "Hans Christian Andersen","title": "Fairy tales", "isbn": "978-1-61-729413-6", "reviews": {} },
+      3: {"author": "Dante Alighieri","title": "The Divine Comedy", "isbn": "978-0-12-345678-9", "reviews": {} },
+      4: {"author": "Unknown","title": "The Epic Of Gilgamesh", "isbn": "978-0-19-852663-6", "reviews": {} },
+      5: {"author": "Unknown","title": "The Book Of Job", "isbn": "978-1-40-289462-9", "reviews": {} },
+      6: {"author": "Unknown","title": "One Thousand and One Nights", "isbn": "978-0-13-609181-3", "reviews": {} },
+      7: {"author": "Unknown","title": "Nj\u00e1l's Saga", "isbn": "978-0-07-013151-4", "reviews": {} },
+      8: {"author": "Jane Austen","title": "Pride and Prejudice", "isbn": "978-1-84-719476-6", "reviews": {} },
+      9: {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "isbn": "978-0-45-152493-5", "reviews": {} },
+      10: {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "isbn": "978-0-59-035342-7", "reviews": {} }
 }
+
 
 module.exports=books;
